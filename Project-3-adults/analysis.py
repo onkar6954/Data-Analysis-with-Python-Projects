1e0b7b5b42d2e112f7794ee5d9a6650c093fb193
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-04-26 23:15:48 +0530

    Check Last 10 Rows of The Dataset

--- Project-3-adults/analysis.py
+++ Project-3-adults/analysis.py
@@ -6,3 +6,6 @@ df = pd.read_csv('Data-Analysis-with-Python-Projects/Project-3-adults/adult.csv'
 
 """ Display Top 10 Rows of The Dataset"""
 # print(df.head(10))
+
+""" Check Last 10 Rows of The Dataset"""
+# print(df.tail(10))

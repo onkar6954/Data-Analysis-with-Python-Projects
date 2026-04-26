b9d743d1a7c4b43f84aafc562fe9e42968a6543e
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-04-26 23:16:51 +0530

    Getting Information About Our Dataset Like Total Number Rows, Total Number of Columns, Datatypes of Each Column And Memory Requirement

--- Project-3-adults/analysis.py
+++ Project-3-adults/analysis.py
@@ -13,3 +13,6 @@ df = pd.read_csv('Data-Analysis-with-Python-Projects/Project-3-adults/adult.csv'
 """ Find Shape of Our Dataset (Number of Rows And Number of Columns)"""
 # print(f"{df.shape[0]} Rows.")
 # print(f"{df.shape[1]} Columns.")
+
+""" Getting Information About Our Dataset Like Total Number Rows, Total Number of Columns, Datatypes of Each Column And Memory Requirement"""
+# print(df.info())

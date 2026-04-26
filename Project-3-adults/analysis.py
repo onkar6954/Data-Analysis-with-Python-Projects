70bddfada2a50634c447c8018b9bab4929047034
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-04-26 23:14:51 +0530

    Display Top 10 Rows of The Dataset

---
+++ Project-3-adults/analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('Data-Analysis-with-Python-Projects/Project-3-adults/adult.csv')
+
+""" Display Top 10 Rows of The Dataset"""
+# print(df.head(10))

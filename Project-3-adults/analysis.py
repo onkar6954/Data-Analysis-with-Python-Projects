2fa5d50b2e498b378f45d9156b8bbcd2713f7a5a
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-04-27 15:25:36 +0530

    Perform Data Cleaning [ Replace '?' with NaN ]

--- Project-3-adults/analysis.py
+++ Project-3-adults/analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 df = pd.read_csv('Data-Analysis-with-Python-Projects/Project-3-adults/adult.csv')
 
@@ -22,6 +23,17 @@ df = pd.read_csv('Data-Analysis-with-Python-Projects/Project-3-adults/adult.csv'
 # print(df.sample(frac=0.5, random_state=100))   # it will give same sequence of random number
 
 """ Check Null Values In The Dataset"""
+# print(df.isnull().sum())
+# sns.heatmap(df.isnull())
+# plt.show()
+
+""" Perform Data Cleaning [ Replace '?' with NaN ]"""
+# print(df.tail(20))
+# print(df.isin(['?']).sum())
+# df['workclass']=df['workclass'].replace('?',np.nan)
+# df['occupation']=df['occupation'].replace('?',np.nan)
+# df['native-country']=df['native-country'].replace('?',np.nan)
+# print(df.isin(['?']).sum())
 # print(df.isnull().sum())
 # sns.heatmap(df.isnull())
 # plt.show()

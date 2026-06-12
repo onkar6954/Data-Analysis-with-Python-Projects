e47d25b3cf8ff2e31bc7b5a590e2d0172d71ce91
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-06-12 15:41:44 +0530

    Find Shape of Our Dataset (Number of Rows & Number of Columns)

--- Project-4-Titanic/analysis.py
+++ Project-4-Titanic/analysis.py
@@ -8,4 +8,8 @@ data = pd.read_csv('titanic.csv')
 # print(data.head())
 
 """ Check the Last 3 Rows of The Dataset """
-print(data.tail(3))
+# print(data.tail(3))
+
+""" Find Shape of Our Dataset (Number of Rows & Number of Columns) """
+print("Number of Rows:",data.shape[0])
+print("Number of Columns:",data.shape[1])
